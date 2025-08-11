c7deb90a97ebd34143e47b36edc7959cdee2c939
sagarshindeXD <156557417+sagarshindeXD@users.noreply.github.com>
2025-08-11 18:55:58 +0530

    final

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 
 try {
   console.log('Starting build process...');
-  // Use node to execute vite from the node_modules directory
+  // Use node to execute vite directly from node_modules
   execSync('node ./node_modules/vite/bin/vite.js build', { stdio: 'inherit' });
   console.log('Build completed successfully!');
 } catch (error) {
